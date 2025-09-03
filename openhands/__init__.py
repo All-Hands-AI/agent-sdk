d01572b7e264b9973025698b6c580e192ff007f6
Xingyao Wang <xingyao@all-hands.dev>
2025-09-03 12:57:55 -0400

    revert

--- openhands/__init__.py
+++ openhands/__init__.py
@@ -1,14 +1,1 @@
-import os
-import pkgutil
-
-
-# Make openhands a namespace package and prefer this repo's path first
-__path__ = pkgutil.extend_path(__path__, __name__)
-try:
-    this_dir = os.path.dirname(__file__)
-    if this_dir in __path__:
-        # Prefer this repository's openhands path
-        __path__.insert(0, __path__.pop(__path__.index(this_dir)))
-except Exception:
-    # Be forgiving if pkgutil changes type; it's fine to proceed without reordering
-    pass
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
