f9343354c9b571d6829041a4983a8d8a5905bd8e
Calvin Smith <calvin@all-hands.dev>
2025-09-10 09:31:45 -0600

    typing on example fixed

--- examples/10_context_condenser.py
+++ examples/10_context_condenser.py
@@ -38,9 +38,9 @@ llm = LLM(
 # Tools
 cwd = os.getcwd()
 tools: list[Tool] = [
-    BashTool(working_dir=cwd),
-    FileEditorTool(),
-    TaskTrackerTool(save_dir=cwd),
+    BashTool.create(working_dir=cwd),
+    FileEditorTool.create(),
+    TaskTrackerTool.create(save_dir=cwd),
 ]
 
 # Create LLM Summarizing Condenser
