ab1539436e725cfd7a71ce6877662677020689c7
Tim O'Farrell <tofarr@gmail.com>
2025-09-22 10:51:36 -0600

    WIP

---
+++ openhands/agent_server/workspace_service.py
@@ -0,0 +1,12 @@
+from openhands.tools.execute_bash.definition import ExecuteBashAction
+
+
+class WorkspaceService:
+    def upload(self, path: str):
+        pass
+
+    def download(self, path: str):
+        pass
+
+    def execute_command(self, action: ExecuteBashAction):
+        pass
