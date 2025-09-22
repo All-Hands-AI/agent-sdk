ee8fff6b5114abc1cbe0cce4771018b22e9de45a
Tim O'Farrell <tofarr@gmail.com>
2025-09-22 13:28:43 -0600

    Agent Server / Webhook examples

---
+++ examples/21_agent_server.py
@@ -0,0 +1,12 @@
+"""
+Script which demonstrates the use of the agent server.
+
+Start a conversation at:
+http://localhost:8000/docs#/default/start_conversation_conversations__post
+"""
+
+from openhands.agent_server.__main__ import main
+
+
+if __name__ == "__main__":
+    main()
