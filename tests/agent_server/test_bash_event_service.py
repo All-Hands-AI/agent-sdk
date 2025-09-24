bf19a47498a4c900b75462a6e7661b16fcc74bb4
Tim O'Farrell <tofarr@gmail.com>
2025-09-24 13:52:50 -0600

    Removed file

--- tests/agent_server/test_bash_event_service.py
+++
@@ -1,4 +0,0 @@
-"""Tests for BashEventService - placeholder file."""
-
-# This file has been cleared as the tests were moved to test_bash_service.py
-# for better organization and comprehensive testing of bash command execution.
